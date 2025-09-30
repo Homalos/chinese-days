085e1752c9267125ba984fea1a396bcb9e92e288
Donny <donnymoving@gmail.com>
2025-09-30 17:27:41 +0800

    feat(core): initialize project version
    - Add __version__.py to define initial project version
    - Set version number to0.1- Include project metadata and description

---
+++ chinesedays/__version__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: chinese-days
+@FileName   : __version__.py
+@Date       : 2025/9/30 17:20
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 项目版本号
+"""
+__version__ = "0.0.1"
